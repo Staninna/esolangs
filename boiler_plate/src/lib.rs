48aafeb18ac97454fe55fca1e8102b5df5b32097
Staninna <26458805+Staninna@users.noreply.github.com>
2022-11-05 12:23:24 +0100

    Changed to raw input and added match statement

--- boiler_plate/src/lib.rs
+++ boiler_plate/src/lib.rs
@@ -1,5 +1,5 @@
 // Import
-use std::{env, fs, process};
+use std::{env, fs, io, process};
 
 // Load program from input
 pub fn init_1d(language_name: &str) -> String {
@@ -12,25 +12,54 @@ pub fn init_1d(language_name: &str) -> String {
         // Load program in memory from input
         [flag, input] => {
             // File input
-            if flag == "-f" {
-                program = match fs::read_to_string(input) {
-                    Ok(program) => program,
-                    Err(_) => {
-                        eprintln!("Error: File not found or could not be read");
-                        process::exit(1);
-                    }
-                };
-            }
-            // String input
-            else if flag == "-s" {
-                program = input.to_string();
+            match flag.as_str() {
+                "-f" => {
+                    program = match fs::read_to_string(input) {
+                        Ok(program) => program,
+                        Err(_) => {
+                            eprintln!("Error: File not found or could not be read");
+                            process::exit(1);
+                        }
+                    };
+                }
+                "-s" => program = input.to_string(),
+                "-h" => {
+                    println!("Usage: {} [flag] [input]", language_name);
+                    println!("Flags:");
+                    println!("    -f: File input");
+                    println!("    -s: String input");
+                    println!("    -h: Help");
+                    process::exit(0);
+                }
+                _ => {
+                    eprintln!("Error: Invalid flag");
+                    process::exit(1);
+                }
             }
         }
 
-        // Show help if no input
+        // Get user input
         _ => {
-            println!("Usage: {} [-f file] [-s string] [-h]", language_name);
-            process::exit(0);
+            // Get program from user
+            let mut program = String::new();
+            println!("Input program input `Exit` to exit: ");
+
+            // Main input loop
+            loop {
+                // Get input
+                let mut new_line = String::new();
+                io::stdin()
+                    .read_line(&mut new_line)
+                    .expect("Failed to read line");
+
+                // Check if input is exit
+                if new_line.to_lowercase().trim() == "exit" {
+                    break;
+                }
+
+                // Add new line to program
+                program.push_str(&new_line);
+            }
         }
     }
 
